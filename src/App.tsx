f9edfc67571bd071ad1397ab7b45f9210155bf71
mrscorchedsilk <139257502+mrscorchedsilk@users.noreply.github.com>
2025-05-16 20:16:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import BusinessOwnersLanding from "./pages/BusinessOwnersLanding";
 import VolatilityLanding from "./pages/VolatilityLanding";
@@ -18,17 +17,17 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/business-owners" element={<BusinessOwnersLanding />} />
           <Route path="/volatility" element={<VolatilityLanding />} />
           <Route path="/emerging-sector" element={<EmergingSectorLanding />} />
           <Route path="/thank-you" element={<ThankYou />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* CATCH-ALL ROUTE MUST BE LAST */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
